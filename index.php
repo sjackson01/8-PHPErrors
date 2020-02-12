d77ee48999b4e751b41fdc043e9d3c48bf11efee
Steven Jackson <stevenpjackson0@gmail.com>
2020-02-12 13:35:55 -0600

    Modify (display_errors.php>Hunk 1) : Change name from index.php too display_errors.php. Add (index.php>Hunk 1) : Create custom my_error_handler function.

--- index.php
+++ index.php
@@ -1,23 +1,44 @@
 <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Displaying Errors</title>
-</head>
-<body>
-<h2>Testing Display Errors</h2>
-<?php # Script 8.1 - display_errors.php
-
-// Show errors:
-ini_set('display_errors', 1);
-
-// Adjust error reporting:
-error_reporting(E_ALL); 
-
-// Create errors:
-foreach ($var as $v) {}
-$result = 1/0;
-
-?>
-</body>
-</html>
+  <html lang="en">
+  <head>
+      <meta charset="utf-8">
+      <title>Handling Errors</title>
+   </head>
+   <body>
+   
+   <h2>Testing Error Handling</h2>
+  <?php # Script 8.3 - handle_errors.php
+  
+  // Flag variable for site status:
+  define('LIVE', TRUE);
+  
+  // Create the error handler:
+  function my_error_handler($e_number, $e_message, $e_file, $e_line, $e_vars) {
+  
+     // Build the error message:
+     $message = "An error occurred in script '$e_file' on line $e_line: $e_message\n";
+  
+     // Append $e_vars to $message:
+     $message .= print_r ($e_vars, 1);
+  
+     if (!LIVE) { // Development (print the error).
+        echo '<pre>' . $message . "\n";
+        debug_print_backtrace();
+        echo '</pre><br>';
+     } else { // Don't show the error.
+        echo '<div class="error">A system error occurred. We apologize for the
+          inconvenience.</div><br>';
+     }
+  
+  } // End of my_error_handler() definition.
+  
+  // Use my error handler:
+  set_error_handler('my_error_handler');
+  
+  // Create errors:
+  foreach ($var as $v) {}
+  $result = 1/0;
+  
+  ?>
+  </body>
+  </html>
